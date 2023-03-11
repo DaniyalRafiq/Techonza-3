6b5c5d435e0d02239935b539e260f359d31fd55a
bb420f@gmail.com <bb420f@gmail.com>
2023-03-11 10:28:10 +0500

    hogya hn header complete without any color

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
     $(".search-icon").click(function (e) {
-        $(this).children('.fa').toggleClass('fa-minus').toggleClass('fa-search');
+        $(this).children('.fa').toggleClass('fa-times').toggleClass('fa-search');
         $('.header-form').toggleClass("active");
         $('.header-form').children('input').focus()
+        $('.search-icon').toggleClass("active");
     });
+
+    $('.menu, menu_close').click(function () {
+        $(".menu").toggleClass("active");
+        $('.mobile_menu').toggleClass("active");
+        $(this).children('.fa').toggleClass('fa-bars').toggleClass('fa-times');
+     })
+
 })
